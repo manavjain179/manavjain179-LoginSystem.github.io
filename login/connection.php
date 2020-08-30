6f54a643700b9afcdf182b7e164145f3392e0449
manavjain179 <59159721+manavjain179@users.noreply.github.com>
2020-08-30 11:51:42 +0000

    Add files via upload

---
+++ login/connection.php
@@ -0,0 +1,8 @@
+<?php      
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "login";  
+      
+    $con = mysqli_connect($host, $user, $password, $db_name) or die('Error Connecting');  
+?>  
